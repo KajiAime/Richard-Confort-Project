f88b91d50fb40ef66ba4b3cc28a277af66081e3a
Nde-Ebom <ndekaji@gmail.com>
2022-08-12 18:50:43 +0100

    The header was not properly formatted.
    The additional_headers can only take one of either \r or \n characters. The headers at the top of sent emails just have to respect this.

--- hand.php
+++ hand.php
@@ -2,21 +2,23 @@
 $to = 'richardcomfort@gmail.com';
 $subject = 'Email from your website.';
 $name = $_POST['name'];
-$headers = "From: $name\r\n\r\n";
-$headers.='Content-type: text/plain; charset: utf-8';
+$headers = "MIME-Version: 1.0"."\r\n";
+$headers .='Content-type: text/plain; charset: utf-8'."\r\n";
+$headers .= "From: $name\r\n";
 $message = "Name: $name\r\n\r\n";
 $email=filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
 if($email){
-    $headers.="\r\n\r\nReply_to: $email";
+    $headers.="Reply_to: $email";
+    $message.="Email: $email\r\n\r\n";
 }
-$message = "Town: ".$_POST['town']."\r\n\r\n";
+$message.= "Town: ".$_POST['town']."\r\n\r\n";
 $message.= "Message: ".$_POST['msg'];
 $sent = mail($to,$subject,$message,$headers,'-f'.$to);
 ?>
 <html>
     <head>
         <title></title>
-        <meta http-equiv="refresh" content="20; URL='contact.htm'"/>
+        <meta http-equiv="refresh" content="3; URL='contact.htm'"/>
     </head>
     <body>
         <?php if(isset($sent) && $sent){?>
